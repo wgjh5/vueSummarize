b40575035bd3933faa6ccfe3c4cd5e014700f5d1
wgjh5 <854453495@qq.com>
2019-04-08 15:56:05 +0800

    新增vue2.x到3.x的桥接和vue移动端仿苹果拖拽事件

--- vuejs 移动端 实现div拖拽移动/app/src/main.js
+++ vuejs 移动端 实现div拖拽移动/app/src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+import VueRouter from 'vue-router'
+	Vue.use(VueRouter)
+const routes = [{
+		path: '/App',
+		name: 'App',
+		component: App
+	},
+	]
 new Vue({
+	VueRouter,
   render: h => h(App),
 }).$mount('#app')
